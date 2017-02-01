34d70b94bbeee8caf64f5e99422d528b24e4d666
haromrozsa <haromrozsa@gmail.com>
2017-02-01 21:51:05 +0100

    Add first 12 lessons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TodoForm, TodoList } from './components/todo';
+import {addTodo, generateId} from './lib/todoHelpers';
 
 class App extends Component {
+ state = {
+    todos: [
+      {id: 1, name: 'Learn JSX', isCompleted: true},
+      {id: 2, name: 'Build an awesome app', isCompleted: false},
+      {id: 3, name: 'Ship it', isCompleted: false},
+    ],
+    currentTodo: ''
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+    const newId = generateId();
+    const newTodo = {id: newId, name: this.state.currentTodo, isCompleted: false}
+    const updatedTodos = addTodo(this.state.todos, newTodo)
+    this.setState({
+      todos: updatedTodos,
+      currentTodo: '',
+      errorMessage: ''
+    })
+  }
+
+  handleEmptySubmit = (evt) => {
+    evt.preventDefault()
+    this.setState({
+      errorMessage: 'Please supply a todo name'
+    })
+  }
+
+  handleInputChange = (evt) => {
+    this.setState({
+      currentTodo: evt.target.value
+    })
+  }
+
   render() {
+    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Todos</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Todo-App">
+          {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
+          <TodoForm handleInputChange={this.handleInputChange}
+            currentTodo={this.state.currentTodo}
+            handleSubmit={submitHandler}/>
+          <TodoList todos={this.state.todos}/>
+        </div>
       </div>
     );
   }
